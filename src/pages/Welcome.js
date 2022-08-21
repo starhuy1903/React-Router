d0d8181bc3a7a5cf4b07865bd5cf2daeaf33b4ad
Huy Nguyen <huykst137@gmail.com>
2022-08-21 21:49:35 +0700

    Nested Routing

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Route} from "react-router-dom";
 
 const Welcome = () => {
     return (
-        <h1>
-            The welcome page
-        </h1>
+        <section>
+            <h1>The welcome page</h1>
+            <Route path="/welcome/new-user">
+                <p>Welcome, new user!</p>
+            </Route>
+        </section>
+
     );
 };
 
