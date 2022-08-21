360877f01ab0a88de605f9e14427efb7dd0ba78c
Huy Nguyen <huykst137@gmail.com>
2022-08-21 20:56:23 +0700

    Adding dynamic Routes with Params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Products = () => {
     return (
-        <h1>
-            The Products page
-        </h1>
+        <section>
+            <h1>The Products page</h1>
+            <ul>
+                <li>A Book</li>
+                <li>A Carpet</li>
+                <li>An Online Course</li>
+            </ul>
+        </section>
     );
 };
 
